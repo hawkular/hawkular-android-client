4aeb86d3a141d66c276a68c71b51c3c3ac9e5d44
Artur Dryomov <artur.dryomov@gmail.com>
2015-06-15 16:40:34 +0300

    Fix license and checkstyle warnings.

--- src/main/java/org/hawkular/client/android/adapter/AlertsAdapter.java
+++ src/main/java/org/hawkular/client/android/adapter/AlertsAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hawkular.client.android.adapter;
 
 import android.content.Context;
@@ -108,7 +124,8 @@ public final class AlertsAdapter extends BindableAdapter<Alert> {
         double alertAverage = getAlertAverageValue(alert);
         double alertThreshold = getAlertThreshold(alert);
 
-        return String.format("Response time was above the threshold (%.1f ms) for %d seconds (until %s). The average response time was %.1f ms.",
+        return String.format("Response time was above the threshold (%.1f ms) for %d seconds (until %s)." +
+                "The average response time was %.1f ms.",
             alertThreshold,
             TimeUnit.MILLISECONDS.toSeconds(alertFinishTimestamp - alertStartTimestamp),
             formatTimestamp(context, alertFinishTimestamp),
