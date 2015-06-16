20ebf1ee31515e8b9c3e576a1b477c5116d5d0c9
Artur Dryomov <artur.dryomov@gmail.com>
2015-06-16 14:22:09 +0300

    Fix license and checkstyle warnings.

--- src/main/java/org/hawkular/client/android/push/PushHandler.java
+++ src/main/java/org/hawkular/client/android/push/PushHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hawkular.client.android.push;
 
 import android.content.Context;
@@ -8,8 +24,7 @@ import org.jboss.aerogear.android.unifiedpush.MessageHandler;
 
 import timber.log.Timber;
 
-public final class PushHandler implements MessageHandler
-{
+public final class PushHandler implements MessageHandler {
     private static final class MessageFields {
         private MessageFields() {
         }
