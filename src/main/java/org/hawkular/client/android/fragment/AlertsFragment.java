4751ffd6135a1222feed8c8c56422523202132ad
Artur Dryomov <artur.dryomov@gmail.com>
2015-08-03 10:12:04 +0300

    Fix checkstyle and license warnings.

--- src/main/java/org/hawkular/client/android/fragment/AlertsFragment.java
+++ src/main/java/org/hawkular/client/android/fragment/AlertsFragment.java
@@ -27,7 +27,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 
