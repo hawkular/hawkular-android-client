3f86398e59bc161107939a486c1a7e8ea0726e37
Anuj Garg <anujgarg.cse@gmail.com>
2016-07-14 19:45:56 +0530

    Add implementaion of fav metrics from Explorer

--- src/main/java/org/hawkular/client/android/explorer/InventoryExplorerActivity.java
+++ src/main/java/org/hawkular/client/android/explorer/InventoryExplorerActivity.java
@@ -17,6 +17,7 @@
 package org.hawkular.client.android.explorer;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.hawkular.client.android.R;
 import org.hawkular.client.android.backend.BackendClient;
@@ -26,15 +27,23 @@ import org.hawkular.client.android.backend.model.Resource;
 import org.hawkular.client.android.explorer.holder.IconTreeItemHolder;
 import org.hawkular.client.android.util.Intents;
 import org.jboss.aerogear.android.pipe.callback.AbstractActivityCallback;
+import org.jboss.aerogear.android.store.DataManager;
+import org.jboss.aerogear.android.store.generator.IdGenerator;
+import org.jboss.aerogear.android.store.sql.SQLStore;
+import org.jboss.aerogear.android.store.sql.SQLStoreConfiguration;
 
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import timber.log.Timber;
 
 /**
@@ -139,18 +148,52 @@ public class InventoryExplorerActivity extends AppCompatActivity {
 
     private TreeNode.TreeNodeLongClickListener nodeLongClickListener = new TreeNode.TreeNodeLongClickListener() {
         @Override
-        public boolean onLongClick(TreeNode node, Object value) {
-            IconTreeItemHolder.IconTreeItem item = (IconTreeItemHolder.IconTreeItem) value;
+        public boolean onLongClick(TreeNode node, final Object value) {
+            final IconTreeItemHolder.IconTreeItem item = (IconTreeItemHolder.IconTreeItem) value;
             if (item.type == IconTreeItemHolder.IconTreeItem.Type.FEED) {
 
             } else if (item.type == IconTreeItemHolder.IconTreeItem.Type.RESOURCE) {
 
             } else if (item.type == IconTreeItemHolder.IconTreeItem.Type.METRIC) {
-
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getInventoryExplorerActivity(), R.style.AlertDialogStyle);
+                builder.setTitle("Hey");
+                builder.setMessage("Do you want to add this Metric to Favourite list?");
+                builder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                addMetricToFav((Metric)item.value);
+                            }
+                        });
+                builder.setNegativeButton("Cancel", null);
+                builder.show();
             }
             return true;
         }
     };
+
+
+    private void addMetricToFav(final Metric metric) {
+        SQLStore<Metric> store = openStore(getApplicationContext());
+        store.openSync();
+        store.save(metric);
+        Toast.makeText(getApplicationContext(), "Metric added to favourite", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private SQLStore<Metric> openStore(Context context) {
+        DataManager.config("Favourite", SQLStoreConfiguration.class)
+                .withContext(context)
+                .withIdGenerator(new IdGenerator() {
+                    @Override
+                    public String generate() {
+                        return UUID.randomUUID().toString();
+                    }
+                }).store(Metric.class);
+        return (SQLStore<Metric>) DataManager.getStore("Favourite");
+    }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
