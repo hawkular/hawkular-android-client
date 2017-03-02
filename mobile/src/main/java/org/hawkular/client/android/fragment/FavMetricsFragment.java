146455a4c89519971ba728599806f97dbc77376f
Anuj Garg <anujgargcse@gmail.com>
2017-03-02 15:43:56 +0530

    Show message when favourite metrics list is empty (#91)

--- mobile/src/main/java/org/hawkular/client/android/fragment/FavMetricsFragment.java
+++ mobile/src/main/java/org/hawkular/client/android/fragment/FavMetricsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+ * Copyright 2015-2017 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -97,9 +97,14 @@ public class FavMetricsFragment extends Fragment implements SwipeRefreshLayout.O
         metrics = new ArrayList<>(array);
         list.setAdapter(new FavMetricsAdapter(getActivity(), this, metrics));
         hideRefreshing();
-        showList();
-    }
 
+        if(metrics.isEmpty()) {
+            showMessage();
+        } else {
+            showList();
+        }
+
+    }
 
     private void hideRefreshing() {
         contentLayout.setRefreshing(false);
@@ -107,6 +112,10 @@ public class FavMetricsFragment extends Fragment implements SwipeRefreshLayout.O
 
     private void showList() {
         ViewDirector.of(this).using(R.id.animator).show(R.id.content);
+    }
+
+    private void showMessage() {
+        ViewDirector.of(this).using(R.id.animator).show(R.id.message);
     }
 
     private SQLStore<Metric> openStore(Context context) {
